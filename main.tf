fbe6994e4804a93ea5a5caf80712f298ebdf3c32
kkatkot <kateruna.kotyh@gmail.com>
2026-02-21 22:47:16 +0200

    Solution 2

--- main.tf
+++ main.tf
@@ -1,58 +1,30 @@
-resource "azurerm_virtual_network" "main" {
-  name                = "${var.prefix}-network"
-  address_space       = ["10.0.0.0/16"]
-  location            = data.azurerm_resource_group.example.location
-  resource_group_name = data.azurerm_resource_group.example.name
-}
+# resource "azurerm_public_ip" "example" {
+#   name                = "${var.prefix}-PublicIp"
+#   resource_group_name = data.azurerm_resource_group.example.name
+#   location            = data.azurerm_resource_group.example.location
+#   allocation_method   = "Static"
+#   sku                 = "Standard"
+# }
 
-resource "azurerm_subnet" "internal" {
-  name                 = "internal"
-  resource_group_name  = data.azurerm_resource_group.example.name
-  virtual_network_name = azurerm_virtual_network.main.name
-  address_prefixes     = ["10.0.2.0/24"]
-  depends_on           = [azurerm_virtual_network.main]
-}
+# resource "azurerm_network_security_group" "example" {
+#   name                = "acceptanceTestSecurityGroup1"
+#   location            = data.azurerm_resource_group.example.location
+#   resource_group_name = data.azurerm_resource_group.example.name
 
-resource "azurerm_network_interface" "main" {
-  name                = "${var.prefix}-nic"
-  location            = data.azurerm_resource_group.example.location
-  resource_group_name = data.azurerm_resource_group.example.name
+#   security_rule {
+#     name                       = "test123"
+#     priority                   = 100
+#     direction                  = "Inbound"
+#     access                     = "Allow"
+#     protocol                   = "Tcp"
+#     source_port_ranges          = ["22", "80"]
+#     destination_port_ranges     = ["22", "80"]
+#     source_address_prefix      = "*"
+#     destination_address_prefix = "*"
+#   }
+# }
 
-  ip_configuration {
-    name                          = "testconfiguration1"
-    subnet_id                     = azurerm_subnet.internal.id
-    private_ip_address_allocation = "Dynamic"
-    public_ip_address_id          = azurerm_public_ip.example.id
-  }
-}
-
-resource "azurerm_public_ip" "example" {
-  name                = "${var.prefix}-PublicIp"
-  resource_group_name = data.azurerm_resource_group.example.name
-  location            = data.azurerm_resource_group.example.location
-  allocation_method   = "Static"
-  sku                 = "Standard"
-}
-
-resource "azurerm_network_security_group" "example" {
-  name                = "acceptanceTestSecurityGroup1"
-  location            = data.azurerm_resource_group.example.location
-  resource_group_name = data.azurerm_resource_group.example.name
-
-  security_rule {
-    name                       = "test123"
-    priority                   = 100
-    direction                  = "Inbound"
-    access                     = "Allow"
-    protocol                   = "Tcp"
-    source_port_range          = "*"
-    destination_port_range     = "*"
-    source_address_prefix      = "*"
-    destination_address_prefix = "*"
-  }
-}
-
-resource "azurerm_network_interface_security_group_association" "example" {
-  network_interface_id      = azurerm_network_interface.main.id
-  network_security_group_id = azurerm_network_security_group.example.id
-}
+# resource "azurerm_network_interface_security_group_association" "example" {
+#   network_interface_id      = data.azurerm_network_interface.main.id
+#   network_security_group_id = azurerm_network_security_group.example.id
+# }
